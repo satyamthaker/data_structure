42cf046fb155c3a0fee5ea20cb8742df1befd542
satyamthaker <60962597+satyamthaker@users.noreply.github.com>
2020-08-09 12:19:39 +0530

    Add files via upload

---
+++ Dequeue.py
@@ -0,0 +1,34 @@
+class Deque:
+    DEFAULT_CAPACITY = 10
+    def __init__(self):
+        self.item = [] * Deque.DEFAULT_CAPACITY
+
+    def isEmpty(self):
+        return self.item == []
+
+    def enqueueFront(self, item):
+        self.item.append(item)
+
+    def enqueueBack(self, item):
+        self.item.insert(0,item)
+
+    def dequeueFront(self):
+        return self.item.pop()
+
+    def dequeueBack(self):
+        return self.item.pop(0)
+
+    def size(self):
+        return len(self.item)
+
+dq=Deque()
+print(dq.isEmpty())
+dq.enqueueBack(9)
+dq.enqueueBack('satyam')
+dq.enqueueFront('thaker')
+dq.enqueueFront(True)
+print(dq.size())
+print(dq.isEmpty())
+dq.enqueueBack(9.9)
+print(dq.dequeueBack())
+print(dq.dequeueFront())
